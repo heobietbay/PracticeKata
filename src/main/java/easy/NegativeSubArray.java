e4af6c76acb01fddd562ba267fee9724b8d1264d
Khoa Tran <trandangkhoa@tma.com.vn>
2017-11-07 20:55:07 +0700

    Hackerrank negative sub array: initial

---
+++ src/main/java/easy/NegativeSubArray.java
@@ -0,0 +1,22 @@
+package easy;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class NegativeSubArray {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+
+        int elementCount = in.nextInt();
+        int[] numArray = new int[elementCount];
+        for(int i = 0 ; i < elementCount; i++)
+        {
+            numArray[i] = in.nextInt();
+        }
+    }
+}

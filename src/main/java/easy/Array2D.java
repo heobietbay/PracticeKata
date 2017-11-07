b1292f58d7ac911f9fa1606c02d0e19fe18f7192
Khoa Tran <trandangkhoa@tma.com.vn>
2017-11-07 16:41:35 +0700

    Solve Hackerrank 2D array

---
+++ src/main/java/easy/Array2D.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Array2D {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.println(max(arr,6,6));
+    }
+
+    static int max(int arr[][], int colLen, int rowLen)
+    {
+        Integer res = null;
+        for(int r = 0 ; r < rowLen - 2; r++)
+        {
+            for(int i=0; i < colLen - 2; i++)
+            {
+                int sum =  arr[r][i] + arr[r][i+1]     + arr[r][i+2]
+                                     + arr[r+1][i+1]
+                         + arr[r+2][i] + arr[r+2][i+1] + arr[r+2][i+2] ;
+
+                if(res == null || sum > res )
+                {
+                    res = sum;
+                }
+            }
+        }
+        return res;
+    }
+}

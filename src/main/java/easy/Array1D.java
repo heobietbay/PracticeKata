f96cb4448f3ca99e3507fd8ae55a512a7ba07b57
Khoa Tran <trandangkhoa@tma.com.vn>
2018-03-25 22:15:00 +0700

    Format comments

--- src/main/java/easy/Array1D.java
+++ src/main/java/easy/Array1D.java
@@ -7,17 +7,23 @@ import java.util.Scanner;
  */
 public class Array1D {
     /*
-    Array is a very simple data structure which is used to store a collection of data, for example roll number of all the students in a class or name of all the countries in the world. To create an array of integer that can hold 10 values, you can write code like this:
+    Array is a very simple data structure which is used to store a collection of data, for example roll number of all
+       the students in a class or name of all the countries in the world.
+    To create an array of integer that can hold 10 values, you can write code like this:
 
     int[] myList = new int[10];
     This problem will test your knowledge on java array.
-    You are given an array of n integers. A sub-array is "Negative" if sum of all the integers in that sub-array is negative. Count the number of "Negative sub-arrays" in the input array.
+    You are given an array of n integers.
+    A sub-array is "Negative" if sum of all the integers in that sub-array is negative. Count the number of "Negative sub-arrays" in the input array.
 
-    Note: Subarrays are contiguous chunks of the main array. For example if the array is {1,2,3,5} then some of the subarrays are {1}, {1,2,3}, {2,3,5}, {1,2,3,5} etc. But {1,2,5} is not an subarray as it is not contiguous.
+    Note: Subarrays are contiguous chunks of the main array.
+    For example if the array is {1,2,3,5} then some of the subarrays are {1}, {1,2,3}, {2,3,5}, {1,2,3,5} etc.
+    But {1,2,5} is not an subarray as it is not contiguous.
 
             Input Format
 
-    The first line consists an integer n. The next line will contain n space separated integers. Value of n will be at most 100. The numbers in the array will range between -10000 to 10000.
+    The first line consists an integer n. The next line will contain n space separated integers.
+    Value of n will be at most 100. The numbers in the array will range between -10000 to 10000.
 
     Output Format
 
--- src/main/java/normal/MaximumSubArray.java
+++ src/main/java/normal/MaximumSubArray.java
@@ -6,10 +6,13 @@ package normal;
 public class MaximumSubArray {
 
     /*
-    In computer science, the maximum subarray problem is the task of finding the contiguous subarray within a one-dimensional array of numbers which has the largest sum. For example,
+    In computer science, the maximum subarray problem is the task of finding the contiguous subarray
+      within a one-dimensional array of numbers which has the largest sum.
+    For example,
     for the sequence of values −2, 1, −3, 4, −1, 2, 1, −5, 4; the contiguous subarray with the largest sum is 4, −1, 2, 1, with sum 6.
 
-    The problem was first posed by Ulf Grenander of Brown University in 1977, as a simplified model for maximum likelihood estimation of patterns in digitized images.
+    The problem was first posed by Ulf Grenander of Brown University in 1977, as a simplified model for maximum likelihood
+      estimation of patterns in digitized images.
     A linear time algorithm was found soon afterwards by Jay Kadane of Carnegie-Mellon University (Bentley 1984).
     */
     public static void main(String[] args) {

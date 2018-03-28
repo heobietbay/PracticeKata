47d1118ed25252ef563784331da98d8a55f0ac77
Khoa Tran <trandangkhoa@tma.com.vn>
2018-03-28 22:20:31 +0700

    Date difference initial writing

---
+++ src/main/java/easy/DateDifference.java
@@ -0,0 +1,87 @@
+package easy;
+
+import java.text.MessageFormat;
+
+/**
+ Create an application that can read in pairs of dates in the following
+ format -
+
+ DD MM YYYY, DD MM YYYY
+
+ Validate the input data, and compute the difference between the two dates
+ in days.
+
+ Output of the application should be of the form -
+
+ DD MM YYYY, DD MM YYYY, difference
+
+ Where the first date is the earliest, the second date is the latest and the difference is the number of days.
+
+ Input can be from a file, or from standard input, as the developer chooses.
+
+ Provide test data to exercise the application.
+
+ Constraints:
+
+ The application may not make use of the platform / SDK libraries for date manipulation
+ (for example Date, Calendar classes).
+
+ The application can limit calculation on an input range of dates from 1900 to 2010
+
+ */
+public class DateDifference {
+
+    public static void main(String[] args) {
+        String input = readInput();
+        boolean isInputValid = isInputValid(input);
+        if(!isInputValid)
+        {
+            System.err.println("Invalid input.");
+            return;
+        }
+        String[] dateStrs = input.split(",");
+        MyDate date1 = fromString(dateStrs[0].trim());
+        MyDate date2 = fromString(dateStrs[1].trim());
+        int daysDifference = calculateDifference(date1,date2);
+
+        String outputTemplate = "{0}, {1}, {2}";
+        //TODO: correct date order
+        System.out.println(MessageFormat.format(outputTemplate,date1.toString(),date2.toString(),daysDifference));
+    }
+
+    private static boolean isInputValid(String input) {
+        return false;
+    }
+
+    private static String readInput() {
+        return SAMPLE;
+    }
+
+    private static int calculateDifference(MyDate date1, MyDate date2) {
+        return 0;
+    }
+
+    private static MyDate fromString(String s) {
+        return null;
+    }
+
+    class MyDate {
+        int day;
+        int month;
+        int year;
+
+        int toDays()
+        {
+            return 0;
+        }
+
+        @Override
+        public String toString() {
+            return "" + day +
+                    " " + month +
+                    " " + year;
+        }
+    }
+
+    static String SAMPLE = "19 02 1988, 20 01 1980";
+}

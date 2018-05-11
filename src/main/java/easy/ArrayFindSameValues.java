899193e166db69773f222762c2ae4d5dc25e50ba
Khoa Tran <trandangkhoa@tma.com.vn>
2018-05-11 15:15:06 +0700

    Arrays intersection finding - Solution when both arrays are sorted.

--- src/main/java/easy/ArrayFindSameValues.java
+++ src/main/java/easy/ArrayFindSameValues.java
@@ -12,6 +12,13 @@ public class ArrayFindSameValues {
         int[] sameValueArray = findSameValues_Better(array1,array2);
 
         System.out.println(Arrays.toString(sameValueArray));
+
+
+        array1 = new int[] {1, 2, 3, 4, 5,10};
+        array2 = new int[] {3,4, 5,6,7,8,9,10};
+        sameValueArray = findSameValues_WhenBothArraysAreSorted(array1,array2);
+
+        System.out.println(Arrays.toString(sameValueArray));
     }
 
     /**
@@ -56,6 +63,38 @@ public class ArrayFindSameValues {
         int[] res = new int[tempLst.size()];
         int idx = 0;
         for(Integer val : tempLst)
+        {
+            res[idx++] = val;
+        }
+        return res;
+    }
+
+    private static int[] findSameValues_WhenBothArraysAreSorted(int[] array1, int[] array2)
+    {
+        Set<Integer> tempSet = new HashSet<>();
+
+        int idxAr1 = 0;
+        int idxAr2 = 0;
+        while(idxAr1 < array1.length && idxAr2 < array2.length){
+            if(array1[idxAr1] < array2[idxAr2])
+            {
+                idxAr1++;
+            }
+            else if(array1[idxAr1] > array2[idxAr2])
+            {
+                idxAr2++;
+            }
+            else
+            {
+                tempSet.add(array1[idxAr1]);
+                idxAr1++;
+                idxAr2++;
+            }
+        }
+
+        int[] res = new int[tempSet.size()];
+        int idx = 0;
+        for(Integer val : tempSet)
         {
             res[idx++] = val;
         }

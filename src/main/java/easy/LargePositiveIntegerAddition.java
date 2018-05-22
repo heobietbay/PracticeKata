6d34ae3283fb2c848bca480a9992b6f2517d7647
Khoa Tran <trandangkhoa@tma.com.vn>
2018-05-22 23:34:35 +0700

    Correct large number string addition

--- src/main/java/easy/LargePositiveIntegerAddition.java
+++ src/main/java/easy/LargePositiveIntegerAddition.java
@@ -3,21 +3,23 @@ package easy;
 public class LargePositiveIntegerAddition {
 
     public static void main(String[] args) {
-        String numStr1 = "1200000";
-        String numStr2 = "77";
+        String numStr1 = "871782912000";
+        String numStr2 = "435891456000";
+        //correct is 1_307_674_368_000
         System.out.println(add(numStr1,numStr2));
     }
-    private static String add(String numStr1,String numStr2)
+    public static String add(String numStr1,String numStr2)
     {
         if(numStr1 == null || numStr1.length() == 0 || numStr2 == null || numStr2.length() == 0)
         {
             throw new IllegalArgumentException("Invalid input number.");
         }
 
-        int carry = 0;
         StringBuilder sb = new StringBuilder();
         // loop from the end of each string, and do basic math addition
-        for(int i = numStr1.length() - 1, j = numStr2.length() - 1; i >= 0 || j >= 0; i--, j--)
+        for(int i = numStr1.length() - 1, j = numStr2.length() - 1, carry = 0;
+            i >= 0 || j >= 0 || carry != 0;
+            i--, j--)
         {
             int num1LastDigit = i < 0 ? 0 : Character.getNumericValue(numStr1.charAt(i));
             int num2LastDigit = j < 0 ? 0 :  Character.getNumericValue(numStr2.charAt(j));

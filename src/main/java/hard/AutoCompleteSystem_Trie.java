e89c983d0cf4b6838a44d5f5373186c030986a33
Khoa Tran <trandangkhoa@tma.com.vn>
2018-06-11 11:20:33 +0700

    Add more problem questions to solve

---
+++ src/main/java/hard/AutoCompleteSystem_Trie.java
@@ -0,0 +1,15 @@
+package hard;
+
+/**
+ * This problem was asked by Twitter.
+ *<p>
+ * Implement an autocomplete system.
+   <pre>That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.</pre>
+ *
+  For example, given the query string <font color='blue'>de</font>
+  and the set of strings <font color='green'>[dog, deer, deal]</font>  return <b>[deer, deal]</b>.
+ *</p>
+ * Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+ */
+public class AutoCompleteSystem_Trie {
+}

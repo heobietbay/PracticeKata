4caf41024bb35232d0f257dab5c876df5016410d
Khoa Tran <trandangkhoa@tma.com.vn>
2018-06-20 16:49:20 +0700

    Initial writing

--- src/main/java/normal/IntersectLinkedList.java
+++ src/main/java/normal/IntersectLinkedList.java
@@ -1,5 +1,7 @@
 package normal;
 
+import core.MySinglyLinkedList;
+
 /**
  * This problem was asked by Google.
  <p>
@@ -14,5 +16,23 @@ package normal;
  */
 public class IntersectLinkedList {
     public static void main(String[] args) {
+        MySinglyLinkedList<Integer> lst1 = createLinkedList(3,7,8,10);
+        MySinglyLinkedList<Integer> lst2 = createLinkedList(99,1,8,10);
+        System.out.println(solution(lst1,lst2));
+    }
+
+    private static Integer solution(MySinglyLinkedList<Integer> lst1, MySinglyLinkedList<Integer> lst2)
+    {
+        //TODO
+        return null;
+    }
+
+    private static MySinglyLinkedList<Integer> createLinkedList(Integer... objs) {
+        MySinglyLinkedList<Integer> linkedList = new MySinglyLinkedList<>();
+        for(Integer i : objs)
+        {
+            linkedList.add(i);
+        }
+        return linkedList;
     }
 }

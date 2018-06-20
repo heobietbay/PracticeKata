985de9cdee9353cdd2e79035ad0f33f7094bd03f
Khoa Tran <trandangkhoa@tma.com.vn>
2018-06-20 09:54:38 +0700

    This problem was asked by Google.
    Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
    
    For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
    
    In this example, assume nodes with the same value are the exact same node objects.
    
    Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.

---
+++ src/main/java/normal/IntersectLinkedList.java
@@ -0,0 +1,18 @@
+package normal;
+
+/**
+ * This problem was asked by Google.
+ <p>
+ *  Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
+ *  <pre>
+ *  For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
+    In this example, assume nodes with the same value are the exact same node objects.
+ </pre>
+ *
+ *  Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+ </p>
+ */
+public class IntersectLinkedList {
+    public static void main(String[] args) {
+    }
+}

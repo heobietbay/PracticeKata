3d2a007093a47dc190b3c9697cbfc01ad5e54091
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-03 19:27:53 +0700

    This problem was asked by Microsoft.
    Compute the running median of a sequence of numbers. That is, given a stream of numbers, print out the median of the list so far on each new element.
    
    Recall that the median of an even-numbered list is the average of the two middle numbers.
    
    For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
    
    2
    1.5
    2
    3.5
    2
    2
    2

---
+++ src/main/java/normal/RunningMedianOfSequenceNumbers.java
@@ -0,0 +1,28 @@
+package normal;
+
+/**
+ * This problem was asked by Microsoft.
+ * <p>
+   Compute the running median of a sequence of numbers.
+   That is, given a stream of numbers, print out the median of the list so far on each new element.
+   Recall that the median of an even-numbered list is the average of the two middle numbers.
+   <pre>
+     For example, given the sequence <b>[2, 1, 5, 7, 2, 0, 5]</b>, your algorithm should print out:
+     2
+     1.5
+     2
+     3.5
+     2
+     2
+     2
+   </pre>
+ </p>
+ *
+ */
+public class RunningMedianOfSequenceNumbers {
+
+    public static void main(String[] args) {
+
+
+    }
+}

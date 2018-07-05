ccb4db0ec728cd1cccad3b74f6296309b29d8fd5
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-05 21:44:28 +0700

    Add more problems
    This problem was asked by Google.
    
    Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
    
    Do this in linear time and in-place.
    
    For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
    
    This problem was asked by Quora.
    
    Given a string, find the palindrome that can be made by inserting the fewest number of characters as possible anywhere in the word. If there is more than one palindrome of minimum length that can be made, return the lexicographically earliest one (the first one alphabetically).
    
    For example, given the string "race", you should return "ecarace", since we can add three letters to it (which is the smallest amount to make a palindrome). There are seven other palindromes that can be made from "race" by adding three letters, but "ecarace" comes first alphabetically.
    
    As another example, given the string "google", you should return "elgoogle".

---
+++ src/main/java/normal/MinInsertPalindrome.java
@@ -0,0 +1,22 @@
+package normal;
+
+/**
+ * This problem was asked by Quora.
+ * <p>
+ *  Given a string, find the palindrome that can be made by inserting the fewest number of characters as possible anywhere in the word.
+ *  If there is more than one palindrome of minimum length that can be made,
+ *  return the lexicographically earliest one (the first one alphabetically).</p>
+ *
+ * <p>
+ *  For example, given the string "race", you should return "ecarace", since we can add three letters to it
+ *  which is the smallest amount to make a palindrome). There are seven other palindromes that can be made from "race"
+ *  by adding three letters, but "ecarace" comes first alphabetically.
+ * </p>
+ * As another example, given the string "google", you should return "elgoogle".
+ */
+public class MinInsertPalindrome {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/normal/RGBSwapping.java
@@ -0,0 +1,18 @@
+package normal;
+
+/**
+ * This problem was asked by Google.
+ *<p>
+  Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first,
+ the Gs come second, and the Bs come last. You can only swap elements of the array.
+ </p>
+  Do this in linear time and in-place.
+ <pre>
+ For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+ </pre>
+ */
+public class RGBSwapping {
+    public static void main(String[] args) {
+
+    }
+}

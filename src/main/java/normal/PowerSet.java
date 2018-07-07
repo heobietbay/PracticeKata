cffa70bad8c1419cdfc71145596db38f67d29051
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-07 21:00:47 +0700

    This problem was asked by Google.
    The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
    
    For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
    
    You may also use a list or array to represent a set.

---
+++ src/main/java/normal/PowerSet.java
@@ -0,0 +1,17 @@
+package normal;
+
+/**
+ * This problem was asked by Google.
+ <p>
+ * The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+ </p>
+ <pre>
+  For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+  You may also use a list or array to represent a set.
+ </pre>
+ */
+public class PowerSet {
+    public static void main(String[] args) {
+
+    }
+}

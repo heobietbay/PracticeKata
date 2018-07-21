e6aa6cedab5b605e029d810c8a9ede71e9475294
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-21 18:47:24 +0700

    Add more problem

---
+++ src/main/java/normal/StringPermutationWithinOtherString.java
@@ -0,0 +1,11 @@
+package normal;
+
+/**
+ * Given a smaller string <font color='blue'>s</font> and a bigger string <b>b</b>, design an algorithm to find
+ * all permutations of the shorter string within the longer one. Print the location of each permutation.
+ */
+public class StringPermutationWithinOtherString {
+    public static void main(String[] args) {
+
+    }
+}

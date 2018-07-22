0b4046ee6c114e715efbbf244d835961e82f9faf
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-22 09:48:55 +0700

    This problem was asked by Google.
    Implement an LRU (Least Recently Used) cache. It should be able to be initialized with a cache size n, and contain the following methods:
    
    set(key, value): sets key to value. If there are already n items in the cache and we are adding a new item, then it should also remove the least recently used item.
    get(key): gets the value at key. If no such key exists, return null.
    
    Each operation should run in O(1) time.

---
+++ src/main/java/normal/LRUCache.java
@@ -0,0 +1,20 @@
+package normal;
+
+/**
+ * This problem was asked by Google.
+ *
+ * Implement an LRU (Least Recently Used) cache.
+ * It should be able to be initialized with a cache size n, and contain the following methods:
+   <pre>
+   set(key, value): sets key to value.
+      If there are already n items in the cache and we are adding a new item, then it should also remove the least recently used item.
+
+   get(key): gets the value at key. If no such key exists, return null.
+  </pre>
+  * Each operation should run in O(1) time.
+ */
+public class LRUCache {
+    public static void main(String[] args) {
+
+    }
+}

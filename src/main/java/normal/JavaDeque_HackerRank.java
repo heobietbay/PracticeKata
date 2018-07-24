38ea753440642b91bdcded118acbb9151f4edd1b
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-24 22:08:03 +0700

    Add more problems

---
+++ src/main/java/normal/JavaDeque_HackerRank.java
@@ -0,0 +1,16 @@
+package normal;
+import java.util.*;
+
+public class JavaDeque_HackerRank {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque<>();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+
+        }
+    }
+}
--- src/main/java/puzzle/SudokuPuzzle.java
+++ src/main/java/puzzle/SudokuPuzzle.java
@@ -4,4 +4,9 @@ package puzzle;
  * Created by khoa on 2/10/2016.
  */
 public class SudokuPuzzle {
+
+    public static boolean isValidSolution(int[][] tiles)
+    {
+        return false;
+    }
 }

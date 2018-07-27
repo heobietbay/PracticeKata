6241b8bd64c2e24211084b8c288eb994053c8dad
Khoa Tran <trandangkhoa@tma.com.vn>
2018-07-27 16:14:00 +0700

    Initial writing

--- src/main/java/normal/JavaDeque_HackerRank.java
+++ src/main/java/normal/JavaDeque_HackerRank.java
@@ -7,10 +7,30 @@ public class JavaDeque_HackerRank {
         Deque deque = new ArrayDeque<>();
         int n = in.nextInt();
         int m = in.nextInt();
-
+        int maxUnique = 0;
+        Set<Integer> uniqueNums = new LinkedHashSet<>(m);
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
+            /*if(!deque.contains(num) //TODO: Improve this
+                    && maxUnique<m)
+            {
+                maxUnique++;
+            }
+            if(deque.size() == m){
+                deque.removeFirst();
+            }
+            deque.addLast(num);*/
 
+            deque.addLast(num);
+            if(deque.size() == m){
+                uniqueNums = new LinkedHashSet<>(deque);
+                if(maxUnique < uniqueNums.size())
+                {
+                    maxUnique = uniqueNums.size();
+                }
+                deque.removeFirst();
+            }
         }
+        System.out.println(""+maxUnique);
     }
 }

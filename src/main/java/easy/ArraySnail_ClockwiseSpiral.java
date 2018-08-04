a025ef1c2cc5673e101a1187716b3e90e4de22b9
Khoa Tran <trandangkhoa@tma.com.vn>
2018-08-04 07:44:33 +0700

    This problem was asked by Amazon.
    Given a N by M matrix of numbers, print out the matrix in a clockwise spiral.
    
    For example, given the following matrix:
    
    [[1,  2,  3,  4,  5],
     [6,  7,  8,  9,  10],
     [11, 12, 13, 14, 15],
     [16, 17, 18, 19, 20]]
    You should print out the following:
    
    1
    2
    3
    4
    5
    10
    15
    20
    19
    18
    17
    16
    11
    6
    7
    8
    9
    14
    13
    12

---
+++ src/main/java/easy/ArraySnail_ClockwiseSpiral.java
@@ -0,0 +1,64 @@
+package easy;
+
+import java.util.List;
+
+/**
+ * This problem was asked by Amazon.
+ <pre>
+ * Given a N by M matrix of numbers, print out the matrix in a clockwise spiral.
+ *
+ * For example, given the following matrix:
+ *
+ * [[1,  2,  3,  4,  5],
+ *  [6,  7,  8,  9,  10],
+ *  [11, 12, 13, 14, 15],
+ *  [16, 17, 18, 19, 20]]
+ * You should print out the following:
+ *
+ * 1
+ * 2
+ * 3
+ * 4
+ * 5
+ * 10
+ * 15
+ * 20
+ * 19
+ * 18
+ * 17
+ * 16
+ * 11
+ * 6
+ * 7
+ * 8
+ * 9
+ * 14
+ * 13
+ * 12
+ </pre>
+ */
+public class ArraySnail_ClockwiseSpiral {
+    /**
+     *
+     <pre>
+     Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+     array = [[1,2,3],
+             [4,5,6],
+             [7,8,9]]
+     snail(array) #=> [1,2,3,6,9,8,7,4,5]
+     For better understanding, please follow the numbers of the next array consecutively:
+     array = [[1,2,3],
+             [8,9,4],
+             [7,6,5]]
+     snail(array) #=> [1,2,3,4,5,6,7,8,9]
+     NOTE: The idea is not sort the elements from the lowest value to the highest;
+     the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+     NOTE 2: The 0x0 (empty matrix) is represented as [[]]
+     </pre>
+     */
+
+    public static List<Integer> snail(List<List<Integer>> array2d)
+    {
+        return null;
+    }
+}

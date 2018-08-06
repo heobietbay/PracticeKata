0b14daa0ba1bb1033918686710500043030963b2
Khoa Tran <trandangkhoa@tma.com.vn>
2018-08-06 23:05:02 +0700

    Done checking horizontal and vertical

--- src/main/java/easy/SearchWordIn2dGrid.java
+++ src/main/java/easy/SearchWordIn2dGrid.java
@@ -21,13 +21,30 @@ import java.util.Arrays;
 public class SearchWordIn2dGrid {
     public static void main(String[] args) {
        char[][] gridCh =  {{'F', 'A', 'C', 'I'},
-               {'O', 'B', 'Q', 'P'},
-               {'A', 'N', 'O', 'B'},
-               {'M', 'A', 'S', 'S'}};
+                           {'O', 'B', 'Q', 'P'},
+                           {'A', 'N', 'O', 'B'},
+                           {'M', 'A', 'S', 'S'}};
+
+        System.out.println( solution(gridCh, "SAM"));
+
+        System.out.println( solution(gridCh, "FACI"));
+        System.out.println( solution(gridCh, "OBQP"));
+        System.out.println( solution(gridCh, "ANOB"));
+        System.out.println( solution(gridCh, "MASS"));
 
         System.out.println( solution(gridCh, "FOAM"));
+        System.out.println( solution(gridCh, "ABNA"));
+        System.out.println( solution(gridCh, "CQOS"));
+        System.out.println( solution(gridCh, "IPBS"));
+
+        System.out.println( solution(gridCh, "FOA"));
         System.out.println( solution(gridCh, "FA"));;
         System.out.println( solution(gridCh, "AC"));
+        System.out.println( solution(gridCh, "BS"));
+        System.out.println( solution(gridCh, "BQ"));
+        System.out.println( solution(gridCh, "OB"));
+        System.out.println( solution(gridCh, "ABN"));
+        System.out.println( solution(gridCh, "ANO"));
     }
 
     public static boolean solution(char[][] gridCh, String word)
@@ -47,12 +64,15 @@ public class SearchWordIn2dGrid {
                         return true;
                 }
                 // vertical check
-                if(row >= word.length())
+                if(row >= word.length() -1 )
                 {
-                    for(int v = row; v>= row - word.length(); v--)
+                    int start = row - (word.length() - 1);
+                    for(int v = start; v < start + word.length(); v++)
                     {
-
+                        tmpCh[v-start] = gridCh[v][col];
                     }
+                    if(Arrays.equals(tmpCh,targetChs))
+                        return true;
                 }
             }
         }

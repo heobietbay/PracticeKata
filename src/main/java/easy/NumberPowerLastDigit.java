36bcf1c5cf1b0357d84b6cd774ec039cacd645d6
Khoa Tran <trandangkhoa@tma.com.vn>
2018-08-26 21:48:13 +0700

    Add more reading articles

--- src/main/java/easy/NumberPowerLastDigit.java
+++ src/main/java/easy/NumberPowerLastDigit.java
@@ -13,7 +13,8 @@ public class NumberPowerLastDigit {
      lastDigit([3, 4, 2]) === 1
      because 3 ^ (4 ^ 2) = 3 ^ 16 = 43046721.
 
-     Beware: powers grow incredibly fast. For example, 9 ^ (9 ^ 9) has more than 369 millions of digits. lastDigit has to deal with such numbers efficiently.
+     Beware: powers grow incredibly fast.
+     For example, 9 ^ (9 ^ 9) has more than 369 millions of digits. lastDigit has to deal with such numbers efficiently.
 
      Corner cases: we assume that 0 ^ 0 = 1 and that lastDigit of an empty list equals to 1.
      */

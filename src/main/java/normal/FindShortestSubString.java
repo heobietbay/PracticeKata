0fcb5f988eb541dec28f36fc9a85e3406dfd5d5e
Khoa Tran <trandangkhoa@tma.com.vn>
2019-03-02 21:36:56 +0700

    This problem was asked by Apple.
    A fixed point in an array is an element whose value is equal to its index. Given a sorted array of distinct elements, return a fixed point, if one exists. Otherwise, return False.
    
    For example, given [-6, 0, 2, 40], you should return 2. Given [1, 5, 7, 8], you should return False.

--- src/main/java/normal/FindShortestSubString.java
+++ src/main/java/normal/FindShortestSubString.java
@@ -1,5 +1,7 @@
 package normal;
 
+import java.util.*;
+
 /**
  * This problem was asked by Square.
  <p>
@@ -14,14 +16,48 @@ public class FindShortestSubString {
     public static void main(String[] args) {
         String case1 = solution("figehaeci", new char[]{'a', 'e', 'i'});
         System.out.println(case1);
-        String case2 = solution("figehaeci", new char[]{'e', 'a', 'i'});
+        String case2 = solution("aecifigeh", new char[]{'e', 'a', 'i'});
         System.out.println(case2);
+        String case3 = solution("cifiaegeh", new char[]{'e', 'a', 'i'});
+        System.out.println(case3);
     }
-
     public static String solution(String inp, char[] charSet)
     {
+        // for each char, find where it appears
+        Map<Character, List<Integer>> characterPosMap = new HashMap<>();
+        for(int charIdx = 0 ; charIdx < charSet.length; charIdx++)
+        {
+            char toFindCh = charSet[charIdx];
+            for(int i = 0 ; i < inp.length(); i++)
+            {
+                char ci = inp.charAt(i);
+                if(toFindCh == ci)
+                {
+                    if(characterPosMap.get(toFindCh) == null)
+                    {
+                        ArrayList<Integer> posLst = new ArrayList<>();
+                        characterPosMap.put(toFindCh, posLst);
+                        posLst.add(i);
+                    }
+                    else
+                    {
+                        characterPosMap.get(toFindCh).add(i);
+                    }
+                }
+            }
+        }
+        // not every character has a position in the input -> return null
+        if(characterPosMap.keySet().size() != charSet.length)
+            return null;
+
+        characterPosMap.forEach( (ch,pos) -> {
+            System.out.println(ch+"_"+ Arrays.toString(pos.toArray()));
+        } );
+        return null;
+    }
+    public static String solutionOld(String inp, char[] charSet)
+    {
         Integer foundIdx = null;
-        StringBuilder res = new StringBuilder();
         Integer rangeStart = null;
         Integer rangeEnd = null;
         for(int charIdx = 0 ; charIdx < charSet.length; charIdx++)

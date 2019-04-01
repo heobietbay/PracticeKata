c21461fc9ff6dc78035885054ae30d6a3d75ca80
Khoa Tran <trandangkhoa@tma.com.vn>
2019-04-01 22:54:22 +0700

    Progress: identify range of nodes that sums to 0.
    NOT DONE YET

--- src/main/java/easy/RemoveSumZeroNodesLinkedList.java
+++ src/main/java/easy/RemoveSumZeroNodesLinkedList.java
@@ -18,9 +18,11 @@ import core.model.Node;
 public class RemoveSumZeroNodesLinkedList {
 
     public static void main(String[] args) {
-        Node<Integer> inputList = buildInput(3 , 4 , -7 , 5 , -6 , 6);
-        solution(inputList);
-        solutionRecursive(inputList,null);
+        Node<Integer> headNode = buildInput(3 , 4 , -7 , 5 , -6 , 6);
+        solutionRecursive(headNode,headNode,null);
+
+        headNode = buildInput(3 , 1 , -10,2,4 , -2 , -6 , 8);
+        solutionRecursive(headNode,headNode,null);
     }
 
     public static void solution(Node<Integer> head)
@@ -28,30 +30,62 @@ public class RemoveSumZeroNodesLinkedList {
 
     }
 
-    public static void solutionRecursive(Node<Integer> head, Integer initialVal)
+    /**
+     * Using recursive.
+     * Use a 'runner node' to proceed from that node til end.
+     * Use a 'originalNode' to keep track from which we start.
+     * <ol>
+     *     <li>
+     *         <pre>IF runnerNode is null and originalNode does not point to any node, meaning we have only have one number but
+     *         without result -> stop there.</pre>
+     *         <code>
+     *             ELSE: Because we have element that link to originalNode, say orinalNodeNext, we continue from there.
+     *         </code>
+     *      </li>
+     *      <li> We start with the HEAD
+     *      </li>
+     *      <li>
+     *          We go from runnerNode til end, each time we sum the node.val.
+     *          <code>
+     *              Any point sum is 0, we KNOW that from originalNode to currentNode is the listNode we can remove.
+     *          </code>
+     *      </li>
+     *      <li>This check every partition from a node til ends to make sure no listNode(that sums to 0) is missed</li>
+     *
+     * </ol>
+     * @param runnerNode   the start point, we go from that til the end.
+     * @param originalNode the original node, that we start from. Use this we can point out list node that sum to 0.
+     * @param initialVal  the sum of node.val
+     */
+    public static void solutionRecursive(Node<Integer> runnerNode,Node<Integer> originalNode, Integer initialVal)
     {
-        if(head == null) {
-            return;
+        if(runnerNode == null) {
+            if(originalNode.getNext() == null)
+              return;
+            else {
+                solutionRecursive(originalNode.getNext(),originalNode,null);
+                return;
+            }
         }
 
         if(initialVal == null){
-            initialVal = head.getVal();
-            solutionRecursive(head.getNext(),initialVal);
+            initialVal = runnerNode.getVal();
+            solutionRecursive(runnerNode.getNext(),runnerNode,initialVal);
         }
         else {
-            initialVal =  initialVal + head.getVal();
+            initialVal =  initialVal + runnerNode.getVal();
             if(initialVal == 0)
             {
-                System.out.println(head.getVal());
+                System.out.println("[" + originalNode.getVal() + "," + runnerNode.getVal() + "]");
+
                 // save the next node
-                Node runner = head.getNext();
-                // remove those node
+                Node runner = runnerNode.getNext();
 
                 //continue
-                solutionRecursive(runner,null);
+                solutionRecursive(runner,originalNode,null);
             }
             else {
-               solutionRecursive(head.getNext(),initialVal);
+               solutionRecursive(runnerNode.getNext(),originalNode,initialVal);
             }
         }
     }

624b5e01db6d7b9ff44b53df36475261e320e0dd
Khoa Tran <trandangkhoa@tma.com.vn>
2019-04-08 23:13:12 +0700

    First attempt using Set to count unique characters. Not very memory efficient and time efficient
    Leetcode grade:
    Runtime: 51 ms, faster than 21.72% of Java online submissions for Longest Substring Without Repeating Characters.
    Memory Usage: 40.2 MB, less than 10.95% of Java online submissions for Longest Substring Without Repeating Characters.

--- src/main/java/normal/LengthOfLongestSubstring.java
+++ src/main/java/normal/LengthOfLongestSubstring.java
@@ -1,5 +1,8 @@
 package normal;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * <pre>
  *     LeetCode.
@@ -29,10 +32,66 @@ package normal;
 public class LengthOfLongestSubstring {
 
     public static void main(String[] args) {
+        int longest = solutionUsingSet("pwwkew");
+        System.out.println(longest);
 
+        longest = solutionUsingSet("abcabcbb");
+        System.out.println(longest);
+
+        longest = solutionUsingSet("bbbbb");
+        System.out.println(longest);
+
+        longest = solutionUsingSet(" ");
+        System.out.println(longest);
+
+        longest = solutionUsingSet("au");
+        System.out.println(longest);
+
+        longest = solutionUsingSet("bwf");
+        System.out.println(longest);
     }
 
-    public static int solution(String s) {
-        return -1;
+    /**
+     * @param s
+     * @return
+     */
+    public static int solutionUsingSet(String s) {
+        if(s.length() < 2)
+            return s.length();
+        int longest = 0;
+        char[] chArray = s.toCharArray();
+
+        Set<Character> tmpChs = new HashSet<>();
+
+        for(int i = 0 , j = i+1;
+            i < chArray.length -1 && j < chArray.length ;
+            j++)
+        {
+            // check the portion from (i , j] for duplicate char
+            for(int idx = i ; idx <= j; idx++) {
+                /*
+                'A': 65
+                'a': 97
+                */
+                Character chTemp = Character.valueOf(chArray[idx]);
+                if(tmpChs.contains(chTemp))
+                {
+                    longest = longest < tmpChs.size() ? tmpChs.size() : longest;
+                    i++;
+                    // reset tmpChs
+                    tmpChs.clear();
+                    break;
+                }
+                else {
+                    tmpChs.add(chTemp);
+                }
+                if(idx == j){
+                    longest = longest < tmpChs.size() ? tmpChs.size() : longest;
+                    // reset tmpChs
+                    tmpChs.clear();
+                }
+            }
+        }
+        return longest;
     }
 }

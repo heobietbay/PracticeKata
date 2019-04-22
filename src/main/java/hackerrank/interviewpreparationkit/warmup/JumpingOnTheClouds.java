6dac3a3dee60b2eb37426fc5f740209cd2b546b2
Khoa Tran <trandangkhoa@tma.com.vn>
2019-04-22 23:08:44 +0700

    Use greedy approach to solve this

--- src/main/java/hackerrank/interviewpreparationkit/warmup/JumpingOnTheClouds.java
+++ src/main/java/hackerrank/interviewpreparationkit/warmup/JumpingOnTheClouds.java
@@ -3,11 +3,33 @@ package hackerrank.interviewpreparationkit.warmup;
 public class JumpingOnTheClouds {
 
     public static void main(String[] args) {
+        int[] clouds = new int[] {0, 0, 1, 0, 0, 1, 0};
+        int minStep = solution(clouds);
+        System.out.println(minStep);
 
+        clouds = new int[] {0, 0, 0, 0, 1, 0};
+        minStep = solution(clouds);
+        System.out.println(minStep);
     }
 
     public static int solution(int[] clouds) {
-
-        return -1;
+        int count = 0;
+        int i = 0;
+        while(i < clouds.length-1)
+        {
+            if( i + JUMP_TWO < clouds.length && clouds[i + JUMP_TWO] != THUNDER_CLOUD){
+                i = i + JUMP_TWO;
+                count++;
+            }
+            else {
+                i++;
+                count++;
+            }
+        }
+        return count;
     }
+
+    private static final int THUNDER_CLOUD = 1;
+    private static final int JUMP_TWO = 2;
+    private static final int JUMP_ONE = 1;
 }

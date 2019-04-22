392b96ad17b745b5baf473d28bfa6fbd345787ed
heobietbay <trandangkhoa@tma.com.vn>
2019-04-22 15:28:53 +0700

    Add "Jumping on clouds" problem

---
+++ src/main/java/hackerrank/interviewpreparationkit/warmup/JumpingOnTheClouds.java
@@ -0,0 +1,13 @@
+package hackerrank.interviewpreparationkit.warmup;
+
+public class JumpingOnTheClouds {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int[] clouds) {
+
+        return -1;
+    }
+}

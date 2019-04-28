3d54378a822aea32cf66ba51fa9ac07c89e0a4d6
Khoa Tran <trandangkhoa@tma.com.vn>
2019-04-28 23:09:23 +0700

    Add daily coding problems package

---
+++ src/main/java/dailycodingproblem/easy/CountPositiveIntegerPairs.java
@@ -0,0 +1,16 @@
+package dailycodingproblem.easy;
+
+/**
+ * This problem was asked by Jane Street.
+ <pre>
+   Given integers M and N, write a program that counts how many positive integer pairs (a, b) satisfy the following conditions:
+ *     a + b = M
+ *     a XOR b = N
+ </pre>
+ */
+public class CountPositiveIntegerPairs {
+
+    public static void main(String[] args) {
+
+    }
+}

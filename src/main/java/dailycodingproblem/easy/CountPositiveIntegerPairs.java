b50874a338b2e22ba5482c7c3095478a9262d692
Khoa Tran <trandangkhoa@tma.com.vn>
2019-05-01 12:49:38 +0700

    Add solution

--- src/main/java/dailycodingproblem/easy/CountPositiveIntegerPairs.java
+++ src/main/java/dailycodingproblem/easy/CountPositiveIntegerPairs.java
@@ -11,6 +11,21 @@ package dailycodingproblem.easy;
 public class CountPositiveIntegerPairs {
 
     public static void main(String[] args) {
+        System.out.println(solution(5,5));
+    }
 
+    public static int solution(int M, int N) {
+        int count = 0;
+        // We can have a starting from 0 to M/2
+        // because a + b = M -> b = M - a
+        // and since (a,b) is same as (b,a), we just need to loop through M/2
+        for(int i = 0 ; i < M / 2; i++)
+        {
+            int a = i;
+            int b = M - a;
+            if( (a^b) == N )
+                count ++;
+        }
+        return count;
     }
 }

a4f973315a7be00d01e02a3b0b98e050bc25ba9e
Khoa Tran <trandangkhoa@tma.com.vn>
2019-05-02 23:05:46 +0700

    Add solution Runtime: 1 ms, faster than 90.07% of Java online submissions for Longest Common Prefix. Memory Usage: 38.9 MB, less than 18.60% of Java online submissions for Longest Common Prefix.

--- src/main/java/leetcode/easy/LongestCommonPrefix.java
+++ src/main/java/leetcode/easy/LongestCommonPrefix.java
@@ -23,9 +23,41 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
         String solution = solution(new String[]{"flower", "flow", "flight"});
         System.out.println(solution);
+
+        solution = solution(new String[]{"dog","racecar","car"});
+        System.out.println(solution);
+
+        solution = solution(new String[]{"aa","a"});
+        System.out.println(solution);
     }
 
+    /**
+     * As the problem state, we need to find the common prefix of ALL element in a array.
+     * So we can just stop at the very first string that does not match the criteria.
+     * We can use the first string as a base, and work from there.
+     * @param strs
+     * @return
+     */
     public static String solution(String[] strs) {
-        return "";
+        if(strs == null || strs.length == 0)
+            return "";
+        StringBuilder sb = new StringBuilder("");
+        String base = strs[0];
+
+        outer:
+        for(int i = 0; i < base.length(); i++)
+        {
+            char ch = base.charAt(i);
+            for (int j = 1; j < strs.length; j++)
+            {
+                String temp = strs[j];
+                if(i >= temp.length())
+                    break outer;
+                if(temp.charAt(i) != ch)
+                    break outer;
+            }
+            sb.append(ch);
+        }
+        return sb.toString();
     }
 }

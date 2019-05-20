623a4aeca3a177989904495800b4d0d1753776aa
heobietbay <trandangkhoa@tma.com.vn>
2019-05-20 11:36:54 +0700

    Initial solution
    Currently use extra space and have lots of duplicated code, needs refactoring

--- src/main/java/leetcode/easy/MergeTwoSortedLinkedList.java
+++ src/main/java/leetcode/easy/MergeTwoSortedLinkedList.java
@@ -22,13 +22,98 @@ public class MergeTwoSortedLinkedList {
 
         Node<Integer> lst2 = createList(1,3,4);
         printList(lst2);
+
+        System.out.println();
+
+        Node<Integer> merge = solution(lst1,lst2);
+        printList(merge);
+
+        System.out.println();
+        lst1 = createList(-9,3);
+        printList(lst1);
+
+        System.out.println();
+
+         lst2 = createList(5,7);
+        printList(lst2);
+
+        System.out.println();
+
+        merge = solution(lst1,lst2);
+        printList(merge);
     }
 
     //TODO
     public static Node<Integer> solution(Node<Integer> lst1,Node<Integer> lst2)
     {
-        Node<Integer> head = lst1.getVal().compareTo(lst2.getVal()) < 0 ? lst1 : lst2;
-        return null;
+        Node<Integer> head = null;
+        Node<Integer> runner = null;
+        while(true)
+        {
+            Integer val1 = lst1 == null ? null : lst1.getVal();
+            Integer val2 = lst2 == null ? null : lst2.getVal();
+
+            if(val1 != null && val2 != null)
+            {
+                int cp = val1.compareTo(val2);
+                if(cp <= 0)
+                {
+                    if(runner == null)
+                    {
+                        runner = new Node<>(val1,null);
+                        head = runner;
+                    }
+                    else {
+                        runner.setNext(new Node<>(val1,null));
+                        runner = runner.getNext();
+                    }
+                    lst1 = lst1.getNext();
+                }
+                else {
+                    if(runner == null)
+                    {
+                        runner = new Node<>(val2,null);
+                        head = runner;
+                    }
+                    else {
+                        runner.setNext(new Node<>(val2,null));
+                        runner = runner.getNext();
+                    }
+                    lst2 = lst2.getNext();
+                }
+            }
+            else if(val1 == null && val2 == null)
+            {
+                break;
+            }
+            else {
+                if(val2 != null) {
+                    if(runner == null)
+                    {
+                        runner = new Node<>(val2,null);
+                        head = runner;
+                    }
+                    else{
+                        runner.setNext(new Node<>(val2,null));
+                        runner = runner.getNext();
+                    }
+                    lst2 = lst2.getNext();
+                }
+                if(val1 != null) {
+                    if(runner == null)
+                    {
+                        runner = new Node<>(val1,null);
+                        head = runner;
+                    }
+                    else{
+                        runner.setNext(new Node<>(val1,null));
+                        runner = runner.getNext();
+                    }
+                    lst1 = lst1.getNext();
+                }
+            }
+        }
+        return head;
     }
 
     static Node<Integer> createList(Integer... els)

9a2c719d6118883487096ab4f887e220c8083e86
heobietbay <trandangkhoa@tma.com.vn>
2019-05-20 13:56:24 +0700

    Two pointer approach solution

--- src/main/java/leetcode/easy/MergeTwoSortedLinkedList.java
+++ src/main/java/leetcode/easy/MergeTwoSortedLinkedList.java
@@ -43,74 +43,58 @@ public class MergeTwoSortedLinkedList {
         printList(merge);
     }
 
-    //TODO
+    /**
+     * Use two pointer approach.
+     * This require O(m+n) space, as we create a new linked list.
+     * @param lst1
+     * @param lst2
+     * @return head of the new linked list.
+     */
     public static Node<Integer> solution(Node<Integer> lst1,Node<Integer> lst2)
     {
         Node<Integer> head = null;
         Node<Integer> runner = null;
         while(true)
         {
-            Integer val1 = lst1 == null ? null : lst1.getVal();
-            Integer val2 = lst2 == null ? null : lst2.getVal();
+            Integer el1 = lst1 == null ? null : lst1.getVal();
+            Integer el2 = lst2 == null ? null : lst2.getVal();
 
-            if(val1 != null && val2 != null)
+            if(el1 != null && el2 != null)
             {
-                int cp = val1.compareTo(val2);
-                if(cp <= 0)
+                int cp = el1.compareTo(el2);
+                Integer val = cp <= 0 ? el1 : el2;
+                if(runner == null)
                 {
-                    if(runner == null)
-                    {
-                        runner = new Node<>(val1,null);
-                        head = runner;
-                    }
-                    else {
-                        runner.setNext(new Node<>(val1,null));
-                        runner = runner.getNext();
-                    }
-                    lst1 = lst1.getNext();
+                    runner = new Node<>(val,null);
+                    head = runner;
                 }
                 else {
-                    if(runner == null)
-                    {
-                        runner = new Node<>(val2,null);
-                        head = runner;
-                    }
-                    else {
-                        runner.setNext(new Node<>(val2,null));
-                        runner = runner.getNext();
-                    }
-                    lst2 = lst2.getNext();
+                    runner.setNext(new Node<>(val,null));
+                    runner = runner.getNext();
                 }
+
+                // move to next element
+                lst1 = cp <= 0 ? lst1.getNext() : lst1;
+                lst2 = cp > 0 ? lst2.getNext() : lst2;
+
             }
-            else if(val1 == null && val2 == null)
+            else if(el1 == null && el2 == null)
             {
                 break;
             }
             else {
-                if(val2 != null) {
-                    if(runner == null)
-                    {
-                        runner = new Node<>(val2,null);
-                        head = runner;
-                    }
-                    else{
-                        runner.setNext(new Node<>(val2,null));
-                        runner = runner.getNext();
-                    }
-                    lst2 = lst2.getNext();
+                Integer val = el2 == null ? el1 : el2;
+                if(runner == null)
+                {
+                    runner = new Node<>(val,null);
+                    head = runner;
                 }
-                if(val1 != null) {
-                    if(runner == null)
-                    {
-                        runner = new Node<>(val1,null);
-                        head = runner;
-                    }
-                    else{
-                        runner.setNext(new Node<>(val1,null));
-                        runner = runner.getNext();
-                    }
-                    lst1 = lst1.getNext();
+                else{
+                    runner.setNext(new Node<>(val,null));
+                    runner = runner.getNext();
                 }
+                lst2 = el2 != null ? lst2.getNext() : lst2;
+                lst1 = el1 != null ? lst1.getNext() : lst1;
             }
         }
         return head;

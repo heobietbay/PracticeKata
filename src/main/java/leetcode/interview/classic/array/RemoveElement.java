9a981ac2f7c2772c262c36d9307032522c2a958b
heobietbay <trandangkhoa@tma.com.vn>
2019-06-07 17:49:48 +0700

    Add TwoPointers solutions

--- src/main/java/leetcode/interview/classic/array/RemoveElement.java
+++ src/main/java/leetcode/interview/classic/array/RemoveElement.java
@@ -48,14 +48,67 @@ public class RemoveElement {
         printArr(nums,newLen);
 
         nums = new int[] {2,2,3};
-        newLen = solution(nums,2);
+        newLen = solution_TwoPointers(nums,3);
         printArr(nums,newLen);
 
         nums = new int[] {0,1,2,2,3,0,4,2};
-        newLen = solution(nums,2);
+        newLen = solution_TwoPointers_WhenElementToRemoveAreRare(nums,1);
         printArr(nums,newLen);
     }
 
+    /**
+     * <pre>
+     We can keep two pointers i and j, where i is the slow-runner while j is the fast-runner.
+     When nums[j] equals to the given value, skip this element by incrementing j.
+     As long as nums[j]!​= val, we copy nums[j] to nums[i] and increment both indexes at the same time.
+     Repeat the process until j reaches the end of the array and the new length is i.
+     * </pre>
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int solution_TwoPointers(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * <pre>
+     Now consider cases where the array contains few elements to remove.
+     For example, nums = [1,2,3,5,4], val = 4.
+     The previous algorithm will do unnecessary copy operation of the first four elements.
+     Another example is nums=[4,1,2,3,5], val = 4.
+     It seems unnecessary to move elements [1,2,3,5] one step left as the problem description mentions that the order of elements could be changed.
+
+     When we encounter nums[i] = val, we can swap the current element out with the last element and dispose the last one.
+     This essentially reduces the array's size by 1.
+     Note that the last element that was swapped in could be the value you want to remove itself.
+     But don't worry, in the next iteration we will still check this element.
+     * </pre>
+     * @param nums
+     * @param val
+     * @return
+     */
+    public static int solution_TwoPointers_WhenElementToRemoveAreRare(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
     /**
      * First solution. 2 main steps
      * <ol>

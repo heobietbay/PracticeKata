ec138cd7ef36208047365360e5c4c92f0f928dcc
Khoa Tran <khoa.tran@gbst.com>
2022-10-12 14:23:56 +0700

    StringPalindrome_MinimumRemoval

---
+++ src/main/java/codility/StringPalindrome_MinimumRemoval.java
@@ -0,0 +1,27 @@
+package codility;
+
+/**
+ * You are given a string S containing lowercase English letters.
+ * Your task is to calculate the minimum number of letters that need to be removed
+   in order to make it possible to build a palindrome from the remaining letters.
+ *  When building the palindrome, you can rearrange the remaining letters in any way.
+ * A palindrome is a string that reads the same both forwards and backwards. Some examples of palindromes are: "kayak", "radar", "mom".
+ * Write a function:
+ * def solution(S)
+ * which, given a string of length N, returns the minimum number of letters that need to be removed.
+ * Examples:
+ * 1. Given S = "ervervige", your function should return 2. After removing the letter "g" and one "e", we may create a word "reviver", which is a palindrome.
+ * 2. Given S = "aaabab, your function should return 0. We may create a word "aabbaa", which is a palindrome and uses all the letters.
+ * 3. Given S = "x", your function should return 0. String "x" is a palindrome itself, so we do not have to delete any letter.
+ * Write an efficient algorithm for the following assumptions:
+ * N is an integer within the range [1..200,000):
+ * S contains only lowercase English letters.
+ */
+public class StringPalindrome_MinimumRemoval {
+    public static void main(String[] args) {
+
+    }
+    public static int solution(String inp) {
+        return 0;
+    }
+}

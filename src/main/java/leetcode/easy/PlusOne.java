f2dd03384e93af0eeb0bb8230f565c3447f3f591
heobietbay <trandangkhoa@tma.com.vn>
2022-11-03 23:48:54 +0700

    PlusOne

--- src/main/java/leetcode/easy/PlusOne.java
+++ src/main/java/leetcode/easy/PlusOne.java
@@ -1,5 +1,10 @@
 package leetcode.easy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
  * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
@@ -31,4 +36,54 @@ package leetcode.easy;
  * Thus, the result should be [1,0].
  */
 public class PlusOne {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        Integer[] addedArr = solution(array);
+        String result = toStr(addedArr);
+        System.out.println(result);
+
+        int[] array2 = {4, 3, 2, 1};
+        addedArr = solution(array2);
+        result = toStr(addedArr);
+        System.out.println(result);
+
+        int[] array3 = {9};
+        addedArr = solution(array3);
+        result = toStr(addedArr);
+        System.out.println(result);
+
+        int[] array4= {9,9};
+        addedArr = solution(array4);
+        result = toStr(addedArr);
+        System.out.println(result);
+
+    }
+    static Integer[] solution(int[] digits) {
+        int[] tmps = Arrays.copyOf(digits, digits.length);
+        List<Integer> lst = new ArrayList<>();
+        int carrier = 1;
+        for(int i = tmps.length -1; i >= 0 ; i--) {
+            int res = tmps[i] + carrier;
+            if(res >= 10) {
+                carrier = 1;
+                res -= 10;
+            } else {
+                carrier = 0;
+            }
+            tmps[i] = res ;
+        }
+        if(carrier == 1) {
+            lst.add(0, carrier);
+        }
+        for(int i: tmps) {
+            lst.add(i);
+        }
+        return lst.toArray(new Integer[0]);
+    }
+
+    private static String toStr(Integer[] addedArr) {
+        return Arrays.stream(addedArr)
+                .map(String::valueOf)
+                .collect(Collectors.joining(""));
+    }
 }

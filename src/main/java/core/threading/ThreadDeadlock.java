f3be8683e96fbc7e095f7ff753fb6112d35aea20
Khoa Tran <tetsusoiwashiro@gmail.com>
2023-11-22 14:59:16 +0700

    New package

--- src/main/java/core/threading/ThreadDeadlock.java
+++ src/main/java/core/threading/ThreadDeadlock.java
@@ -1,4 +1,4 @@
-package core;
+package core.threading;
 
 public class ThreadDeadlock {
     public static void main(String[] args){
--- src/main/java/core/threading/ThreadInterruptBasic.java
+++ src/main/java/core/threading/ThreadInterruptBasic.java
@@ -1,4 +1,4 @@
-package core;
+package core.threading;
 
 import java.util.concurrent.TimeUnit;
 
